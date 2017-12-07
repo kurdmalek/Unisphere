dcbf160fb25451a19864cba99e0d16b22712e852
Saunon Malekshahi <Admin@Admins-MacBook-Pro.local>
2017-12-07 00:55:20 -0800

    tabs fixed

--- wixel-sdk/wixelmaster.py
+++ wixel-sdk/wixelmaster.py
@@ -8,14 +8,14 @@ from wixel import *
 master = wixelMasterController()
 
 while(True):
-    for i in range(0, 6):
-        master.setServoPosition(1, i, 45)
+	for i in range(0, 6):
+		master.setServoPosition(1, i, 45)
 	master.sendSlaveData(0)
-    	time.sleep(2)
-        master.setServoPosition(1, i, -45)
-    	master.sendSlaveData(0)
-    	time.sleep(2)
-        master.setServoPosition(1, i, 0)
-   	master.sendSlaveData(0)          
+		time.sleep(2)
+		master.setServoPosition(1, i, -45)
+		master.sendSlaveData(0)
+		time.sleep(2)
+		master.setServoPosition(1, i, 0)
+	master.sendSlaveData(0)          
 master.closeMaster()
 
