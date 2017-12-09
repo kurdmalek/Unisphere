0835d07c664aae6f9bbcd50f9b8ec60e6ab87722
Saunon Malekshahi <Admin@Admins-MacBook-Pro.local>
2017-12-08 23:43:42 -0800

    master v3 change

--- wixel-sdk/wixelmaster.py
+++ wixel-sdk/wixelmaster.py
@@ -7,14 +7,13 @@ from wixel import *
 
 master = wixelMasterController()
 
-while(True):
-	for i in range(0, 6):
-		master.setServoPosition(0, i, 1000)
+for i in range(0, 6):
+	master.setServoPosition(0, i, 1000)
 	master.sendSlaveData(0)
 	time.sleep(4)
-		master.setServoPosition(0, i, 2000)
+	master.setServoPosition(0, i, 2000)
 	master.sendSlaveData(0)
 	time.sleep(4)
-		master.setServoPosition(0, i, 0)
+	master.setServoPosition(0, i, 0)
 	master.sendSlaveData(0)          
 master.closeMaster()
