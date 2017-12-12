41bf5ad8ad03ead558e7c07ba3177a0b23f92f02
Saunon Malekshahi <smalekshahi@berkeley.edu>
2017-12-12 15:11:53 -0800

    masterv10

--- wixel-sdk/wixelmaster.py
+++ wixel-sdk/wixelmaster.py
@@ -6,17 +6,17 @@ master = wixelMasterController()
 
 while (True):
 	for i in range(0, 6):
-		master.setServoPosition(0, i, 1545)
-	master.sendSlaveData(0)
+		master.setServoPosition(1, i, 1800)
+	master.sendSlaveData(1)
 	time.sleep(4)
 	
 	for i in range(0,6):
-		master.setServoPosition(0, i, 1455)
-	master.sendSlaveData(0)
+		master.setServoPosition(1, i, 1200)
+	master.sendSlaveData(1)
 	time.sleep(4)
 
 	for i in range(0,6):
-		master.setServoPosition(0, i, 0)
-	master.sendSlaveData(0)
+		master.setServoPosition(1, i, 0)
+	master.sendSlaveData(1)
 	time.sleep(4)
 master.closeMaster()
